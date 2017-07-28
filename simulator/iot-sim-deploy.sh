32da6cf18e6b4cee2326619ebc842bae96e61f26
Chris Custine <ccustine@apache.org>
2017-07-28 11:01:46 -0600

    Initial mock data config for simulator
    This will be replaced by the Cloudera generator code soon
    
    Signed-off-by: Chris Custine <ccustine@apache.org>

---
+++ simulator/iot-sim-deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#*******************************************************************************
+# Copyright (c) 2017 Red Hat, Inc and others
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     Red Hat, Inc. - initial API and implementation
+#
+#******************************************************************************
+
+. openshift-common.sh
+
+: ${OPENSHIFT_PROJECT_NAME:=redhat-iot}
+: ${DOCKER_ACCOUNT:=redhatiot}
+
+
+# Set up new simulator instance
+$OC delete dc/simulator
+$OC delete configmap data-simulator-config
+$OC create configmap data-simulator-config --from-file=ksim.simulator.configuration=generators.json -n "$OPENSHIFT_PROJECT_NAME"
+# $OC set env --from=configmap/data-simulator-config dc/simulator -n "$OPENSHIFT_PROJECT_NAME"
+
+$OC new-app -n "$OPENSHIFT_PROJECT_NAME" -f iot-simulator.yml
