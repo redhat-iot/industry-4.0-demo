ff341987d19891c387538e4cdbbf612caf5fc04e
Chris Custine <ccustine@apache.org>
2017-09-07 14:48:42 -0600

    Update Broker host and login
    Signed-off-by: Chris Custine <ccustine@apache.org>

--- dashboard/config.js
+++ dashboard/config.js
@@ -15,10 +15,10 @@
 var config =
 {
     // hostname/port/name/password for Kapua broker
-    BROKER_HOSTNAME: process.env.BROKER_HOSTNAME,
+    BROKER_HOSTNAME: process.env.BROKER_HOSTNAME + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE + '.apps.cloudera-iot-demo.rhiot.org',
     BROKER_WS_PORT: process.env.BROKER_WS_PORT || 80,
-    BROKER_USERNAME: process.env.BROKER_USERNAME || "demo_username",
-    BROKER_PASSWORD: process.env.BROKER_PASSWORD || "demo_password",
+    BROKER_USERNAME: process.env.BROKER_USERNAME || "demo-gw2",
+    BROKER_PASSWORD: process.env.BROKER_PASSWORD || "RedHat123",
 
     // hostname/port for DG proxy (no username/password required for demo)
     DASHBOARD_PROXY_HOSTNAME: process.env.DASHBOARD_PROXY_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE,
